db07f83150321f1f36bc2b8f1238e8260242b194
DESKTOP-UBTA1HQ\JCdev <jhonjoeldev@gmail.com>
2020-09-10 20:44:24 +0800

    create transaction model

---
+++ models/Transaction.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const TransactionSchema = new Schema(
+   {
+      transactionCode: {
+         type: String,
+         required: [true, "Transaction Code Required"],
+      },
+      property: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "Property",
+         required: [true, "Property required"],
+      },
+      user: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "User",
+         required: [true, "Review required"],
+      },
+      bookingDays: {
+         type: Number,
+         required: [true, "Booking Days Required"],
+      },
+      startDate: {
+         type: String,
+         required: [true, "Start Date Required"],
+      },
+      endDate: {
+         type: String,
+         required: [true, "End Date Required"],
+      },
+      total: {
+         type: Number,
+         required: [true, "Total Required"],
+      },
+      status: {
+         type: String,
+         required: [true, "Status Required"],
+         default: "Pending",
+      },
+      paymentMode: {
+         type: String,
+         required: [true, "Status Required"],
+         default: "Pending",
+      },
+   },
+   {
+      timestamps: true,
+      toJSON: { virtuals: true },
+      toObject: { virtuals: true },
+   }
+);
+
+module.exports = mongoose.model("Transaction", TransactionSchema);
